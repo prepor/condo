008859a6e8f2cd4908703d95515e16bc082fad17
Andrew Rudenko <ceo@prepor.ru>
2017-04-02 22:01:12 +0300

    websocket stream API

--- api/api.go
+++ api/api.go
@@ -7,6 +7,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/websocket"
 	negronilogrus "github.com/meatballhat/negroni-logrus"
 	"github.com/prepor/condo/supervisor"
 	"github.com/prepor/condo/system"
@@ -42,11 +43,43 @@ func (x *API) stateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func (x *API) stateStreamHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.WithError(err).Error("Can't upgrade websocket connection")
+		return
+	}
+	done := make(chan struct{})
+	updates := x.stateManager.readStream(done)
+	go func() {
+		if _, _, err := conn.NextReader(); err != nil {
+			close(done)
+		}
+	}()
+	for {
+		update, ok := <-updates
+		if !ok {
+			conn.Close()
+			return
+		}
+
+		if err := conn.WriteJSON(update); err != nil {
+			close(done)
+		}
+	}
+}
+
 func (x *API) worker(system *system.System, address string) error {
 	log.WithField("address", address).Info("Starting HTTP")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/v1/state", x.stateHandler)
+	mux.HandleFunc("/v1/state-stream", x.stateStreamHandler)
 
 	n := negroni.New(negroni.NewRecovery())
 	n.Use(negronilogrus.NewMiddleware())
--- api/state.go
+++ api/state.go
@@ -6,7 +6,9 @@ import (
 )
 
 type stateManager struct {
-	readRequests chan chan<- map[string]instance.Snapshot
+	readRequests      chan chan<- map[string]instance.Snapshot
+	readStreams       chan chan<- *streamAnswer
+	closedReadStreams chan chan<- *streamAnswer
 }
 
 type clbKeyType string
@@ -20,6 +22,9 @@ type newSnapshot struct {
 
 func newStateManager(supervisor *supervisor.Supervisor) *stateManager {
 	readRequests := make(chan chan<- map[string]instance.Snapshot)
+	readStreams := make(chan chan<- *streamAnswer)
+	closedReadStreams := make(chan chan<- *streamAnswer)
+	streams := make([]chan<- *streamAnswer, 0)
 	instances := make(map[string]instance.Snapshot)
 	newInstances := make(chan *instance.Instance)
 	removedInstances := make(chan *instance.Instance)
@@ -53,19 +58,45 @@ func newStateManager(supervisor *supervisor.Supervisor) *stateManager {
 				delete(instances, i.Name)
 			case s := <-snapshots:
 				instances[s.name] = s.snapshot
+				for _, stream := range streams {
+					stream <- &streamAnswer{
+						Name:     s.name,
+						Snapshot: s.snapshot,
+					}
+				}
 			case r := <-readRequests:
 				a := make(map[string]instance.Snapshot)
 				for k, v := range instances {
 					a[k] = v
 				}
 				r <- a
+			case r := <-readStreams:
+				for k, v := range instances {
+					r <- &streamAnswer{
+						Name:     k,
+						Snapshot: v,
+					}
+				}
+				streams = append(streams, r)
+			case s := <-closedReadStreams:
+				newStreams := streams[:0]
+				for _, x := range streams {
+					if x != s {
+						newStreams = append(newStreams, x)
+					} else {
+						close(x)
+					}
+				}
+				streams = newStreams
 			}
 		}
 
 	}()
 
 	return &stateManager{
-		readRequests: readRequests,
+		readRequests:      readRequests,
+		readStreams:       readStreams,
+		closedReadStreams: closedReadStreams,
 	}
 }
 
@@ -73,4 +104,19 @@ func (x *stateManager) readCurrent() map[string]instance.Snapshot {
 	a := make(chan map[string]instance.Snapshot)
 	x.readRequests <- a
 	return <-a
+}
+
+type streamAnswer struct {
+	Name     string
+	Snapshot instance.Snapshot
+}
+
+func (x *stateManager) readStream(done <-chan struct{}) <-chan *streamAnswer {
+	a := make(chan *streamAnswer)
+	x.readStreams <- a
+	go func() {
+		<-done
+		x.closedReadStreams <- a
+	}()
+	return a
 }
