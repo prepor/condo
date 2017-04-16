4bef46bf7c26b27cf556e163c05ef1cd925b50fa
Andrew Rudenko <ceo@prepor.ru>
2017-04-16 12:52:36 +0300

    waitfor API

--- api/api.go
+++ api/api.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/Jeffail/gabs"
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/websocket"
 	negronilogrus "github.com/meatballhat/negroni-logrus"
 	"github.com/prepor/condo/expose"
+	"github.com/prepor/condo/instance"
 	"github.com/prepor/condo/static"
 	"github.com/prepor/condo/supervisor"
 	"github.com/prepor/condo/system"
@@ -132,6 +135,148 @@ func (x *API) stateStreamHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type waitForItem struct {
+	service string
+	image   string
+	state   string
+}
+
+func localDeployedImage(snap instance.Snapshot) string {
+	switch s := snap.(type) {
+	default:
+		return ""
+	case *instance.Stable:
+		return s.Container.Spec.Image()
+	case *instance.WaitNext:
+		return s.Current.Spec.Image()
+	case *instance.Wait, *instance.TryAgain:
+		return ""
+	case *instance.TryAgainNext:
+		return s.Current.Spec.Image()
+	case *instance.BothStarted:
+		return s.Next.Spec.Image()
+	}
+}
+
+func globalDeployedImage(snap interface{}) string {
+	s, err := gabs.Consume(snap)
+	if err != nil {
+		panic(err)
+	}
+	state := s.Path("State").Data().(string)
+	switch state {
+	default:
+		return ""
+	case "Stable":
+		return s.Path("Container.Spec.Image").Data().(string)
+	case "WaitNext", "TryAgainNext":
+		return s.Path("Current.Spec.Image").Data().(string)
+	case "Wait", "TryAgain":
+		return ""
+	case "BothStarted":
+		return s.Path("Next.Spec.Image").Data().(string)
+	}
+}
+
+func (x *API) waitForLocal(service string, image string, done <-chan struct{}, deployed chan<- struct{}) {
+	states := x.stateManager.readStream(done)
+	for {
+		state, ok := <-states
+		if !ok {
+			break
+		}
+		status := true
+		for k, v := range state {
+			if k == service {
+				if localDeployedImage(v) != image {
+					status = false
+					break
+				}
+			}
+		}
+		if status {
+			close(deployed)
+			break
+		}
+	}
+
+}
+
+func (x *API) waitForGlobal(service string, image string, done <-chan struct{}, deployed chan<- struct{}) {
+	states := x.exposer.ReceiveStates(done)
+	for {
+		state, ok := <-states
+		if !ok {
+			break
+		}
+		status := true
+		for _, v := range state {
+			if v.Service == service {
+				if globalDeployedImage(v.Snapshot) != image {
+					status = false
+					break
+				}
+			}
+		}
+		if status {
+			close(deployed)
+			break
+		}
+	}
+
+}
+
+func (x *API) waitFor(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	scope := query.Get("scope")
+	service := query.Get("service")
+	image := query.Get("image")
+
+	if scope == "global" && !x.checkExposer(w) {
+		return
+	}
+
+	timeoutStr := query.Get("timeout")
+
+	if timeoutStr == "" {
+		timeoutStr = "1m"
+	}
+
+	timeoutV, err := time.ParseDuration(timeoutStr)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Can't parse timeout")
+		return
+	}
+
+	timeout := time.After(timeoutV)
+
+	done := make(chan struct{})
+	defer close(done)
+	deployed := make(chan struct{})
+	switch scope {
+	case "local":
+		go x.waitForLocal(service, image, done, deployed)
+	case "global":
+		go x.waitForGlobal(service, image, done, deployed)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Unknown scope")
+		return
+
+	}
+
+	select {
+	case <-timeout:
+		w.WriteHeader(http.StatusGatewayTimeout)
+		fmt.Fprintln(w, "Can't wait for desired state")
+	case <-deployed:
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, "{}")
+	}
+}
+
 func (x *API) worker(system *system.System, address string) error {
 	log.WithField("address", address).Info("Starting HTTP")
 	mux := http.NewServeMux()
@@ -141,6 +286,8 @@ func (x *API) worker(system *system.System, address string) error {
 
 	mux.HandleFunc("/v1/global-state", x.globalStateHandler)
 	mux.HandleFunc("/v1/global-state-stream", x.globalStateStreamHandler)
+
+	mux.HandleFunc("/v1/wait-for", x.waitFor)
 
 	if os.Getenv("LIVE_UI") == "" {
 		mux.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(static.HTTP)))
